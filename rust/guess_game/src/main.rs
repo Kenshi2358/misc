91bf70b71d232046f45a9d59e2b2b666944e53fa
Scott <scott.hahnert@sapphire-digital.com>
2024-05-06 15:27:36 -0400

    Moved comments to the top and completed guess_game

--- rust/guess_game/src/main.rs
+++ rust/guess_game/src/main.rs
@@ -1,3 +1,27 @@
+/*
+General Notes:
+
+Call the thread_rng() function and use the gen_range method().
+gen_range is inclusive on the lower and upper bounds.
+
+read_line puts what the user enters into a string.
+read_line returns a Result value. A Result is an enumeration and is often called an enum.
+An enum can be one of multiple possible states and each possible state is called a variant.
+
+Result's variants are Ok and Err. The Ok variant indicates that the operation was successful.
+The Err variant means that the operation failed.
+We want to crash the program when a problem occurs, so we are using the expect method.
+
+The parse method returns a Result type and Result is an enum with variants: Ok and Err.
+
+Convert user_guess1 string to an unsigned 32-bit integer.
+Trim any whitespace with the trim() method. Parse any carriage returns iwth the parse() method.
+
+The cmp method compares 2 values and can be called on anything that can be compared.
+Ordering is another enum and has variants: Less, Greater, and Equal.
+
+*/
+
 // Importing the io and cmp libraries from the standard library.
 use std::io;
 use std::cmp::Ordering;
@@ -7,22 +31,12 @@ use rand::Rng;
 
 fn main() {
 
-    println!("Guess the number!");
+    println!("Guess the number! -- Guess a number between 1 and 100 --");
 
-    // Call the thread_rng() function and use the gen_range method().
-    // gen_range is inclusive on the lower and upper bounds.
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
+    //println!("The secret number is: {secret_number}");
 
-
-    /* read_line puts what the user enters into a string.
-    read_line returns a Result value. A Result is an enumeration and is often called an enum.
-    An enum can be one of multiple possible states and each possible state is called a variant.
-
-    Result's variants are Ok and Err. The Ok variant indicates that the operation was successful.
-    The Err variant means that the operation failed.
-    We want to crash the program when a problem occurs, so we are using the expect method. */
-
+    let mut num_guesses = 1;
     loop {
 
         println!("Enter your guess:");
@@ -32,8 +46,6 @@ fn main() {
             .read_line(&mut user_guess1)
             .expect("Failed to read line");
 
-        // Convert user_guess1 string to an unsigned 32-bit integer.
-        // Trim any whitespace with the trim() method. Parse any carriage returns iwth the parse() method.
         let user_guess1: u32 = match user_guess1.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
@@ -41,15 +53,15 @@ fn main() {
 
         println!("You guessed: {user_guess1}");
 
-        // The cmp method compares 2 values and can be called on anything that can be compared.
-        // Ordering is another enum and has variants: Less, Greater, and Equal.
         match user_guess1.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("Too small! # guesses: {num_guesses}"),
+            Ordering::Greater => println!("Too big! # guesses: {num_guesses}"),
             Ordering::Equal => {
-                println!("You win!");
+                println!("You win! # guesses: {num_guesses}");
                 break;
             }
         }
+
+        num_guesses += 1;
     }
 }
