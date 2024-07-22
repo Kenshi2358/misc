ed4f10ea1f702ae953441c0fce68185b1975edd9
Scott Hahnert <shahnert@zlsjd256m5xqn.prod.zelis.com>
2024-07-22 11:38:18 -0400

    Updating ssh1 file to use colorama and sort dictionary

--- ssh1.py
+++ ssh1.py
@@ -2,25 +2,56 @@
 Script to ssh onto servers faster.
 """
 import os
+from colorama import Fore, Style
 
 server_container = {
-    'f': 'fury',
     'v': 'vulture',
-    't': 'thanos',
-    'k': 'kang',
-    'a': 'abomination',
     'gg': 'greengoblin',
+    't': 'thanos',
     'u': 'ultron',
+
     'd': 'drax',
+    'f': 'fury',
+    'gr': 'groot',
     'ga': 'gamora',
+    'i': 'ironman',
+
     'r': 'rocket',
-    'w': 'wolverine',
+    's': 'spiderman',
     'ws': 'wintersoldier',
-    'm': 'magneto'
+    'w': 'wolverine',
+    'm': 'magneto',
+
+    've': 'venom',
+    'sh': 'shangchi',
+
+    'a': 'abomination',
+    'k': 'kang'
 }
 
-user_answer1 = input(f'Choose from the server_container:\n{server_container}\n')
+server_str1 = f"""
+    provider dev:{Fore.CYAN}
+    v - vulture, gg - greengoblin
+    t - thanos, u - ultron{Style.RESET_ALL}
 
+    provider prod:{Fore.GREEN}
+    d - drax, f - fury
+    gr - groot, ga - gamora
+    i - ironman, r - rocket
+    s - spiderman, ws - wintersoldier
+    w - wolverine, m - magneto{Style.RESET_ALL}
+
+    member dev:{Fore.YELLOW}
+    ve - venom{Style.RESET_ALL}
+
+    member prod:{Fore.MAGENTA}
+    sh - shangchi{Style.RESET_ALL}
+
+    misc:{Fore.LIGHTRED_EX}
+    a - abomination, k - kang{Style.RESET_ALL}
+    """
+
+user_answer1 = input(f'Choose from the server_container:\n{server_str1}\n')
 
 for key, value in server_container.items():
     if user_answer1 == key:
