d8e197ca996741adb8c1f452cd4c86bdb88e93e3
Scott Hahnert <scott.hahnert@sapphire-digital.com>
2024-07-23 16:55:46 -0400

    Trying static flask app - not working

---
+++ flask/static/app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, render_template
+
+current_file_path = os.path.abspath(__file__)
+root_dir = os.path.dirname(current_file_path)
+
+app = Flask(__name__)
+# app = Flask(__name__, template_folder="/Users/shahnert/Desktop/Repos/misc/flask/static/templates")
+# app.config["EXPLAIN_TEMPLATE_LOADING"] = True
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+print(root_dir)
+
+if __name__ == '__main__':
+    app.run(debug=True)
