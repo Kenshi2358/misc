6ab2730a9dbd3a6ba405cf525ed2ae780960783b
Scott2358 <104395699+Scott2358@users.noreply.github.com>
2024-08-13 10:47:28 -0400

    Updating z_test1.py to toggle tabs

--- test_py/z_test1.py
+++ test_py/z_test1.py
@@ -7,7 +7,7 @@ import pyautogui
 
 # Get current mouse x and y:
 # print(pyautogui.position())
-# Get current screen resolution width and height --> 1920, 1080.
+# Get current screen resolution width and height.
 # print(pyautogui.size())
 # pyautogui.typewrite("hello world")
 
@@ -31,8 +31,18 @@ for i in range(total_loops):
     millisecond_random_num = random.randint(0, 1000) / 1000
 
     total_wait = current_random_num + millisecond_random_num
+    delta_wait = total_wait - 1
 
-    # print(f'# of seconds to sleep: {total_wait}')
+    random_tab_num = random.randint(1, 4)
+    toggle_num = str(random_tab_num)
 
-    time.sleep(total_wait)
+    pyautogui.keyDown('command')
+    pyautogui.keyDown(toggle_num)
+    time.sleep(1)
+    pyautogui.keyUp('command')
+    pyautogui.keyUp(toggle_num)
+
+    # print(f'# of seconds to sleep: {total_wait} delta wait: {delta_wait}')
+
+    time.sleep(delta_wait)
     num_loops_completed += 1
