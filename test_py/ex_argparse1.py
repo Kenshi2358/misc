53eee3dba75db2e5e150e93d77aa1d069973c2d2
Scott2358 <104395699+Scott2358@users.noreply.github.com>
2024-10-16 12:05:48 -0400

    ex_argparse.py: Updated instructions in the multi-doc strings to better explain

--- test_py/ex_argparse1.py
+++ test_py/ex_argparse1.py
@@ -1,10 +1,13 @@
-'''
-Example of why using argparse with optional function parameters does not work.
-The function fails to convert encoding to "utf-8" because None type is still an object type according to argparse.
+"""
+Argparse example:
+You cannot use optional functionl parameters for an argparse parameter that is not filled out.
+The non-filled out argparse parameter defaults to None. None gets passed to the function and stays as None.
+None type is still an object type according to argparse.
 
-To correct, use argparses's default option when its not being used.
-
-'''
+To correct, use argparses's default option when it's not being used.
+For example:
+parser.add_argument("-e", "--encoding", type=str, help="File encoding", required=False)
+"""
 import argparse
 import logging
 
@@ -14,9 +17,11 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 
+
 def filewrite(encoding='utf-8'):
 
     logging.info(f'Encoding type: {encoding}')
+
 
 if __name__ == '__main__':
 
