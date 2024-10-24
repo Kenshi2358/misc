c4cb18388270e7924e9c109e0525fc251e8c5c57
Scott2358 <104395699+Scott2358@users.noreply.github.com>
2024-10-24 10:26:12 -0400

    Uncommenting string named index example

--- test_py/ex_string_formatting.py
+++ test_py/ex_string_formatting.py
@@ -2,16 +2,16 @@
 # name = 'Scott'
 # pet = 'dog'
 
-# # Oldest method: %-formatting.
-# print('My name is %s and I like to walk my %s.' %(name, pet))
+# Oldest method: %-formatting.
+print('My name is %s and I like to walk my %s.' %(name, pet))
 
-# # Newer method: string formatting.
-# print('My name is {} and I like to walk my {}.'.format(name, pet))
-# # string formatting using named indexes.
-# print('My name is {current_name} and I like to walk my {current_pet}.'.format(current_name=name, current_pet=pet))
+# Newer method: string formatting.
+print('My name is {} and I like to walk my {}.'.format(name, pet))
+# string formatting using named indexes.
+print('My name is {current_name} and I like to walk my {current_pet}.'.format(current_name=name, current_pet=pet))
 
-# # Newest method: f-string example.
-# print(f'My name is {name} and I like to walk my {pet}.')
+# Newest method: f-string example.
+print(f'My name is {name} and I like to walk my {pet}.')
 
 
 # Example of using the split method.
