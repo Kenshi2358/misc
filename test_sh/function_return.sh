bd0d7207d87592df39ad212fd44f8269de7295b5
Scott2358 <104395699+Scott2358@users.noreply.github.com>
2024-11-01 14:34:45 -0400

    Updating function_return.sh example with better indentation and comments

--- test_sh/function_return.sh
+++ test_sh/function_return.sh
@@ -9,25 +9,24 @@ get_column_code_string() {
     local code="$2"
     local alternate_value="${3:-}"
 
+    # Optional parameter check.
     local value=$column_name
     if [ ! -z "${alternate_value}" ]; then
         value=$alternate_value
     fi
 
     local result="Column: ${column_name}, Code: ${code} value: ${value} alternate_value: ${alternate_value}"
-    
     echo "$result"
 }
 
 # function parameters: column_name, code, and an optional alternate_value.
 returned_string=$(get_column_code_string "$column" "$code" "dog")
-echo "Returned String: $returned_string"
+echo "Returned String: $returned_string\n"
 
 fel_select="some_text_"
+
+# function parameters: column_name, code.
 fel_select+=$(get_column_code_string "$column" "$code")
 echo "fel_select: $fel_select"
 
 echo "\ncolumn: ${column} code: ${code}"
-
-# Shows how to take the output and store it in a file.
-# echo "$fel_select" > file1.txt
