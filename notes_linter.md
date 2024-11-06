8070cb020de2e95d74cc154c4828edcbf5cf0d0c
Scott2358 <104395699+Scott2358@users.noreply.github.com>
2024-11-06 16:46:54 -0500

    Ran linter on ex_regular_expressions.py, added E402 to ruff ignore, added ruff to notes_linter.md

---
+++ notes_linter.md
@@ -0,0 +1,4 @@
+To review a file with the ruff linter, type: `ruff check filename`
+
+To review all files in the current directory, type: `ruff check`
+
--- test_py/ex_regular_expressions.py
+++ test_py/ex_regular_expressions.py
@@ -1,22 +1,23 @@
 
 import re
 
-#each_header = '"some_column_name"'
-each_header = 'some_column_name'
+each_header = "first_name"
+#each_header = '5first_name#$#$%'
 
-# Check that the column names begin with a letter or an underscore.
+# Check that the column name begins with a letter or an underscore.
 first_character = each_header[0]
-print(re.findall("[a-zA-Z_]", first_character))
+
 if re.findall("[a-zA-Z_]", first_character):
-    print("This column name begins with a valid character.")
+    print(f"\nHeader: {each_header} begins with a valid character, which is: {first_character}")
 else:
-    column_err_str = (f"Header: {each_header} does not begin with a letter or an underscore. This is an invalid column name.\n")
-    column_err_str += ("Please check your header and try again.")
-    print(f"{column_err_str}")
+    column_err = f"\nHeader: {each_header} does not begin with a letter or an underscore."
+    column_err += "\nThis is an invalid column name.\n"
+    column_err += "Please check your header and try again."
+    print(f"{column_err}")
 
 # Check that subsequent characters in the name are letters, numbers, or underscores.
 if not re.match(r'^[a-zA-Z_0-9 ]+$', each_header):
-    column_err_str = (f"Header: {each_header} contains characters that are not letters, numbers, or underscores. This is an invalid column\n")
-    column_err_str += ("Please check your header and try again.")
-    print(f"{column_err_str}")
-
+    column_err = f"\nHeader: {each_header} contains characters that are not letters, numbers, or underscores."
+    column_err += "\nThis is an invalid column name.\n"
+    column_err += "Please check your header and try again."
+    print(f"{column_err}")
