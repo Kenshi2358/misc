9d3c126856e5d9895d0ba4f9c3dc91a9896ac003
Scott2358 <104395699+Scott2358@users.noreply.github.com>
2024-11-11 16:20:19 -0500

    Adding debug example

---
+++ test_py/ex_debug.py
@@ -0,0 +1,6 @@
+"""
+There are 2 ways to set a breakpoint:
+1) breakpoint()
+2) Going into your IDE and setting a breakpoint manually on a line of code.
+This potentially requires adding additional code, such as pass as well.
+"""
