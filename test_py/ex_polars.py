4f781bda98e0e6105ff625844bbc9597ee98cd03
Scott2358 <104395699+Scott2358@users.noreply.github.com>
2024-12-10 11:22:27 -0500

    Adding large csv example for ex_polars.py

--- test_py/ex_polars.py
+++ test_py/ex_polars.py
@@ -1,5 +1,7 @@
 # Standard Library imports
+import argparse
 import inspect
+import math
 import os
 import sys
 
@@ -16,6 +18,52 @@ sys.path.insert(1, parent_dir)
 # Custom imports
 import settings
 from settings import logger
+
+
+def get_total_num_batches(args) -> None:
+    """
+    Gets the total # of batches found in the file and prints it out
+    """
+    full_file_name = f"{args.local_path}{args.filename}"
+
+    reader = pl.read_csv_batched(source=full_file_name, separator="|", infer_schema_length=0, low_memory=True)
+    item_count = 0
+    while True:
+        batches = reader.next_batches(1)
+        if not batches:
+            break
+        item_count += 1
+    logger.info(f"Total # of batches in this file: {item_count}")
+
+
+def write_to_database2(df: pl.DataFrame, table_name: str, connection_uri: str) -> None:
+    logger.info("Starting table write")
+
+    try:
+        # Load 50,000 records as a time.
+        chunk_size = 50_000
+
+        num_chunks = math.ceil(len(df) / chunk_size)
+        logger.info(f"{num_chunks} chunks to load this batch.")
+
+        chunk_count = 1
+        for i in range(0, len(df), chunk_size):
+            chunk = df[i:i+chunk_size]
+
+            chunk.write_database(
+                table_name=table_name,
+                connection=connection_uri,
+                if_table_exists='append',  # 'replace' to truncate and reload, 'fail' to fail if exists, 'append' to append
+                engine='sqlalchemy'
+            )
+            logger.info(f"chunk # {chunk_count} written to database")
+            chunk_count += 1
+
+        logger.info("All data written for this batch")
+
+    except Exception as e:
+        logger.error(f"Error writing to database: {e}")
+        sys.exit(1)
 
 
 def write_to_database(df: pl.DataFrame, table_name: str, connection_uri: str) -> None:
@@ -35,34 +83,90 @@ def write_to_database(df: pl.DataFrame, table_name: str, connection_uri: str) ->
         logger.error(f"Error writing to database: {e}")
         sys.exit(1)
 
-current_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-# Create an empty dataframe with 1 column defined.
-df1 = pl.DataFrame(schema={"column_id": pl.Int64})
+def main(args):
+    """
+    Main procedure for run.
+    """
 
-temp_table_name = ""
-connection_uri = ""
-type_code = ""
-file_list = []
+    current_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-for each_file in file_list:
+    # Create an empty dataframe with 1 column defined.
+    df1 = pl.DataFrame(schema={"column_id": pl.Int64})
 
-    # Read a csv into a polars dataframe.
-    temp_file_path = ""
-    temp_df = pl.read_csv(temp_file_path, encoding='windows-1252', infer_schema_length=None, separator=',', quote_char='"')
+    temp_table_name = ""
+    connection_uri = f"postgresql://mdx@{args.server}:5432/{args.database}"
+    type_code = ""
+    file_list = []
+    small_csv = True
 
-    # Update column names to lowercase. Update spaces to underscore.
-    temp_df = temp_df.rename({col: col.lower().replace(" ", "_") for col in temp_df.columns})
+    for each_file in file_list:
 
-    # Write each dataframe to it's own table.
-    write_to_database(temp_df, temp_table_name, connection_uri)
+        if small_csv:
+            # =======================
+            # Small CSV file to load:
+            # Read a csv into a polars dataframe.
+            temp_file_path = ""
+            temp_df = pl.read_csv(temp_file_path, encoding='windows-1252', infer_schema_length=None, separator=',', quote_char='"')
 
-    # Update temp_df with 2 new columns.
-    temp_df = temp_df.with_columns([
-        pl.lit(type_code).alias("type_code1"),
-        pl.lit(current_timestamp).alias("file_download")
-    ])
+            # Update column names to lowercase. Update spaces to underscore.
+            temp_df = temp_df.rename({col: col.lower().replace(" ", "_") for col in temp_df.columns})
 
-    # Read each file into the same dataframe.
-    df1 = pl.concat([df1, temp_df], how='align')
+            # Write each dataframe to it's own table.
+            write_to_database(temp_df, temp_table_name, connection_uri)
 
+        else:
+            # =======================
+            # Large CSV file to load:
+
+            # Keeping in case we need to do data manipulation before loading.
+            # Can run sql queries after loading.
+
+            # # Dry run the reader to get the total # of batches.
+            get_total_num_batches(args)
+
+            reader = pl.read_csv_batched(source=each_file, separator="|", infer_schema_length=0, low_memory=True)
+            batches = reader.next_batches(5)
+            batch_count = 5
+            print_first_header = True
+
+            while batches:
+                logger.info(f"Batch #'s {batch_count - 4} to {batch_count}")
+                df_current_batches = pl.concat(batches)
+                if print_first_header:
+                    logger.info(df_current_batches.head(3))
+                print_first_header = False
+
+                write_to_database(df_current_batches, args.table_name, connection_uri)
+                batches = reader.next_batches(5)
+                batch_count += 5
+            # =======================
+
+        # Update temp_df with 2 new columns.
+        temp_df = temp_df.with_columns([
+            pl.lit(type_code).alias("type_code1"),
+            pl.lit(current_timestamp).alias("file_download")
+        ])
+
+        # Read each file into the same dataframe.
+        df1 = pl.concat([df1, temp_df], how='align')
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    # Required parameters
+    parser.add_argument("-s", "--server", type=str, help="the server in which the database exists", required=True)
+    parser.add_argument("-d", "--database", type=str, help="the database in which the table will be created", required=True)
+    parser.add_argument("-f", "--filename", type=str, help="the file to be parsed and inserted into a table", required=True)
+    parser.add_argument("-t", "--table_name", type=str, help="name of table to be created", required=True)
+    parser.add_argument("-sc", "--schema", type=str, help="schema where you want to table to be created", required=True)
+
+    # Optional parameters
+    parser.add_argument("-e", "--encoding", type=str, help="client's file", default="utf-8")
+    parser.add_argument("-z", "--target_size_mb", type=int, help="Target file size of each split file in megabytes", default=2000)
+
+    global_args = parser.parse_args()
+
+    main(global_args)
