649ca7bbacd6d0326dff937120e77e7540c57e10
Scott2358 <104395699+Scott2358@users.noreply.github.com>
2024-12-17 13:57:40 -0500

    Cleaning up flask folder and separating by project name

--- flask/proj1/app.py
+++ flask/proj1/app.py
@@ -4,9 +4,9 @@ from flask import Flask, render_template
 current_file_path = os.path.abspath(__file__)
 root_dir = os.path.dirname(current_file_path)
 
-app = Flask(__name__)
-# app = Flask(__name__, template_folder="/Users/shahnert/Desktop/Repos/misc/flask/static/templates")
-# app.config["EXPLAIN_TEMPLATE_LOADING"] = True
+# app = Flask(__name__)
+app = Flask(__name__, template_folder="/Users/shahnert/Desktop/Repos/misc/flask/proj1/templates")
+app.config["EXPLAIN_TEMPLATE_LOADING"] = True
 
 @app.route("/")
 def index():
