23920e59b89062d3a6af039dfcbfbbbc9ac7dae2
Scott2358 <104395699+Scott2358@users.noreply.github.com>
2025-01-07 18:37:34 -0500

    updated memory_check.py

--- test_py/memory_check.py
+++ test_py/memory_check.py
@@ -1,7 +1,7 @@
 from pympler import asizeof
 
-number_to_check = 2489787
-my_list = [i for i in range(number_to_check)]
+number_to_check = 100_000
+my_list = [f"{i}I'm a dog. My name is Logan. Bark bark. I love to eat and go for runs." for i in range(number_to_check)]
 total_size = asizeof.asizeof(my_list)
 
 print(f"Total memory size: {total_size:,} bytes")
