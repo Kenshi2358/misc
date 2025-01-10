4f98d95ae51983f360345479407b81bf60d378f8
Scott2358 <104395699+Scott2358@users.noreply.github.com>
2025-01-10 11:39:18 -0500

    Added convert_bytes function to memory_check.py

--- test_py/memory_check.py
+++ test_py/memory_check.py
@@ -1,7 +1,18 @@
 from pympler import asizeof
 
+def convert_bytes(bytes, units=['bytes', 'KB', 'MB', 'GB', 'TB']):
+    """
+    returns a human readable string representation of bytes
+    """
+    if bytes < 1024:
+        return f"{bytes:.2f} {units[0]}"
+    else:
+        return convert_bytes((bytes/1024.0 * 1.0), units[1:])
+
+
 number_to_check = 300_000
 my_list = [f"{i}I'm a dog. My name is Logan. Bark bark. I love to eat and go for runs." for i in range(number_to_check)]
 total_size = asizeof.asizeof(my_list)
 
-print(f"Total memory size: {total_size:,} bytes")
+bytes_str = convert_bytes(total_size)
+print(f"Total memory size: {total_size:,} bytes, which is {bytes_str}")
