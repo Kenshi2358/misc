c3e05da087bf31b859f8445d630936d26ddf7988
Scott2358 <104395699+Scott2358@users.noreply.github.com>
2025-01-10 11:02:50 -0500

    updating memory_check.py

--- test_py/memory_check.py
+++ test_py/memory_check.py
@@ -1,6 +1,6 @@
 from pympler import asizeof
 
-number_to_check = 100_000
+number_to_check = 300_000
 my_list = [f"{i}I'm a dog. My name is Logan. Bark bark. I love to eat and go for runs." for i in range(number_to_check)]
 total_size = asizeof.asizeof(my_list)
 
